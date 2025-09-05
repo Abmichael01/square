9571dab10c09de8098c4b628c4a993a5d990c9fc
Urkelcodes <abmichael109@gmail.com>
2025-09-05 22:43:33 +0100

    any

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Use WhiteNoise for static files in production
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
